4364ea236cecb3c64d2c3c72abba00353be48d74
Deer-Leeon <lj.buchmiller@gmail.com>
2024-12-02 15:01:19 -0700

    made the website look slick

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ app.set("view engine", "ejs");
 
 app.set("views", path.join(__dirname, "views")); //assigns views folder for index.js to point to
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.urlencoded({extended: true}));
+
+
 
 //Connect to pgAdmin
 const knex = require("knex") ({
@@ -23,8 +27,8 @@ const knex = require("knex") ({
     connection : {
         host : "localhost",
         user : "postgres",
-        password : "FatnessEverd33n", //My password
-        database : "starwars", //selecting the database I want
+        password : "password", //My password
+        database : "database", //selecting the database I want
         port : 3207 //change this to 3207!! (can find port in pg admin under properties, then connection)
     }
 });
@@ -36,5 +40,48 @@ const knex = require("knex") ({
 app.get('/', (req, res) => { 
     res.render("index")
 });
+
+// Add these to your existing index.js
+app.get('/admin', (req, res) => { 
+    res.render("admin")
+});
+
+app.get('/hostForm', (req, res) => { 
+    res.render("hostForm")
+});
+
+app.get('/jen', (req, res) => { 
+    res.render("jen")
+});
+
+app.get('/volunteerForm.ejs', (req, res) => { 
+    res.render("volunteerForm.ejs")
+});
+
+app.get('/difference', (req, res) => { 
+    res.render("difference")
+});
+
+app.get('/donate', (req, res) => { 
+    res.render("donate")
+});
+
+app.get('/ownEvent', (req, res) => { 
+    res.render("ownEvent")
+});
+
+app.get('/volunteer', (req, res) => { 
+    res.render("volunteer")
+});
+
+app.get('/partner', (req, res) => { 
+    res.render("partner")
+});
+
+app.get('/accomplishments', (req, res) => { 
+    res.render("accomplishments")
+});
+
+// Add similar routes for partner, volunteer, ownEvent, accomplishments, admin
 
 app.listen(port, () => console.log(`Node.js is listening`));
