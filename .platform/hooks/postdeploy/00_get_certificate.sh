988e8c97b893fbd0085a1a53af8f3d5f0e20e305
tklipp8 <turnerklippel@gmail.com>
2024-12-05 11:54:41 -0700

    fixed folders

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -3,7 +3,7 @@
 # Add your domains here
 DOMAINS=(
   "turtle-dev-env.eba-tcisefdb.us-east-1.elasticbeanstalk.com"
-  "turtleshelterprojectdev.com"
+  "turtleshelterprojectdev.is404.net"
 )
 
 # Join domains with -d flag
@@ -14,7 +14,3 @@ done
 
 # Run certbot with the constructed domain arguments and --expand flag
 sudo certbot -n --nginx $DOMAIN_ARGS --expand --agree-tos --email turnerklippel@gmail.com
-
-
-
-sudo certbot -n -d  --nginx --agree-tos --email lk273@byu.edu
