9338611cda25e6e9edb8efd43fa57f4aee0377df
tklipp8 <turnerklippel@gmail.com>
2024-12-05 12:25:38 -0700

    fixed dns record

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-# Add your domains here
-DOMAINS=(
-  "turtle-dev-env.eba-tcisefdb.us-east-1.elasticbeanstalk.com"
-  "turtleshelterprojectdev.is404.net"
-)
-
-# Join domains with -d flag
-DOMAIN_ARGS=""
-for DOMAIN in "${DOMAINS[@]}"; do
-  DOMAIN_ARGS="$DOMAIN_ARGS -d $DOMAIN"
-done
-
-# Run certbot with the constructed domain arguments and --expand flag
-sudo certbot -n --nginx $DOMAIN_ARGS --expand --agree-tos --email turnerklippel@gmail.com
+sudo certbot -n -d tspd.is404.net --nginx --agree-tos --email turnerklippel@gmail.com
