9b8117ee5bdbcc573232d65c83792e5852787e1e
tklipp8 <turnerklippel@gmail.com>
2024-12-05 11:14:35 -0700

    changing domains

--- .platform/postdeploy/00_get_certificate.sh
+++ .platform/postdeploy/00_get_certificate.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-
-# Add your domains here
-DOMAINS=(
-  "turtle-dev-env.eba-tcisefdb.us-east-1.elasticbeanstalk.com"
-  "turtleshelterprojectdev.com"
-)
-
-# Join domains with -d flag
-DOMAIN_ARGS=""
-for DOMAIN in "${DOMAINS[@]}"; do
-  DOMAIN_ARGS="$DOMAIN_ARGS -d $DOMAIN"
-done
-
-# Run certbot with the constructed domain arguments and --expand flag
-sudo certbot -n --nginx $DOMAIN_ARGS --expand --agree-tos --email lk273@byu.edu
+sudo certbot -n -d turtleshelterprojectdev.com --nginx --agree-tos --email lk273@byu.edu
