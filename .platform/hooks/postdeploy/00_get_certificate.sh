10457eeaee569ac1e3644f1630837f8ed5e96a52
tklipp8 <turnerklippel@gmail.com>
2024-12-05 19:52:07 -0700

    RUN AWAY!!!!!

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-# Add your domains here
-DOMAINS=(
-  "turt.us-east-1.elasticbeanstalk.com"
-  "turtleshelterprojectdev.com"
-)
-
-# Join domains with -d flag
-DOMAIN_ARGS=""
-for DOMAIN in "${DOMAINS[@]}"; do
-  DOMAIN_ARGS="$DOMAIN_ARGS -d $DOMAIN"
-done
-
-# Run certbot with the constructed domain arguments and --expand flag
-sudo certbot -n --nginx $DOMAIN_ARGS --expand --agree-tos --email turnerklippel@gmail.com
+sudo certbot -n -d turtleshelterprojectdev.com --nginx --agree-tos --email turnerklippel@gmail.com
