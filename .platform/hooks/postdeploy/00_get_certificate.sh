f382f0dd725c1fe2def58a9610892d162d04b0ec
tklipp8 <turnerklippel@gmail.com>
2024-12-05 19:33:18 -0700

    changing out beanstalk

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-sudo certbot -n -d turtleshelterprojectdev.com --nginx --agree-tos --email turnerklippel@gmail.com
+# Add your domains here
+DOMAINS=(
+  "turtleshelterprojectdev.com"
+  "turtleshelterprojectdev.com"
+)
+
+# Join domains with -d flag
+DOMAIN_ARGS=""
+for DOMAIN in "${DOMAINS[@]}"; do
+  DOMAIN_ARGS="$DOMAIN_ARGS -d $DOMAIN"
+done
+
+# Run certbot with the constructed domain arguments and --expand flag
+sudo certbot -n --nginx $DOMAIN_ARGS --expand --agree-tos --email turnerklippel@gmail.com
+
+
+#!/bin/bash
+
+sudo certbot -n -d  --nginx --agree-tos --email turnerklippel@gmail.com
